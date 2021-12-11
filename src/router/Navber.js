390088c429c53d03640e84cf1c31aae7a5c8583e
fkrhkd507 <ourinon00@gmail.com>
2021-12-11 17:25:28 +0900

    todolist input, api add

--- src/router/Navber.js
+++ src/router/Navber.js
@@ -1,43 +1,32 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function Navber(){
 
-    const [value, setValue] = React.useState(0);
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
 
     const a_Style = {
         padding: "0 10px",
-        borderRight: "1px solid black"
+
     }
         
     const wrap_Style = {
         height: "50px",
-        background: "bisque",
         display: "flex",
         alignItems: "center",
-        border: "3px solid black"
     }
 
     return(
-        <Box className="navber_wrap" style={wrap_Style}>
-        <Tabs
-            onChange={handleChange}
-            value={value}
-            aria-label="Tabs where selection follows focus"
-            selectionFollowsFocus
-        />
-
-        <Link to="/todo" style={a_Style}><Tab label="Todolist"></Tab></Link>
-        <Link to="/drawing" style={a_Style}><Tab label="drawing"></Tab></Link>
-        <Link to="/Game" style={a_Style}><Tab label="Game"></Tab></Link>
-
-        </Box>
+        <>
+            <div className="navber_wrap" style={wrap_Style} sw={{width: '100%'}}>
+                <Stack direction="row" spacing={2}>
+                    <Link to="/todo" value="Todolist" style={a_Style} label="Todolist"><Button>Todolist</Button></Link>
+                    <Link to="/drawing" value="drawing" style={a_Style} label="drawing"><Button>Drawing</Button></Link>
+                    <Link to="/Game" value="Game" style={a_Style} label="Game"><Button>Game</Button></Link>
+                </Stack>
+            </div>
+        </>
     );
 }
 
--- src/router/Todolist.js
+++ src/router/Todolist.js
@@ -8,13 +8,54 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import TextField from '@mui/material/TextField';
 
 function Todolist(){
-    let [weather, weather_change] = useState(0);
+    let [weather, change_weather] = useState(0);
+    let [weather_icon, change_weather_icon] = useState();
+    let [weather_description, change_weather_description] = useState();
 
-    setInterval(() => {
-        weather_change(weather, Get_weather());
-    }, 10000);
+    function Show_weather(){
+        if(!navigator.geolocation){
+            change_weather("X");
+            return;          
+        }
+    
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                Get_weather(position.coords.latitude, position.coords.longitude)
+                .then((weather) => {
+                    console.log(weather);
+                    change_weather(weather.temper);
+                    change_weather_icon(weather.icon);
+                    change_weather_description(weather.description);
+                });
+            }
+        );
+    };
+
+    async function Get_weather(lat, lon){
+        let weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=kr&appid=c8899082aee3315b60daa2a1da712285`)
+        .then((res) => {return res.json()})
+        .then((json) => {
+            let weather = {
+                temper : ((json.main.temp - 273.15).toFixed(0)),
+                icon : `http://openweathermap.org/img/w/${json.weather[0].icon}.png`,
+                description : json.weather[0].description
+            };
+
+            return weather;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+        return weather;
+    }
+
+    useEffect(() => {
+        Show_weather();
+    }, [weather]);
 
     return(
         <div className="wrap">
@@ -25,42 +66,43 @@ function Todolist(){
                         <AppBar position="static">
                             <Toolbar>
                                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                                    <div className="weather">weather : {weather}</div>
+                                    <div className="weather">
+                                        <span className="temper">{weather}&#8451;</span> 
+                                        <span className="weather_desc">{weather_description}</span> 
+                                        <span className="weather_icon"><img src={weather_icon}></img></span>
+                                        </div>
                                 </Typography>
-                                <Button color="inherit">Login</Button>
+                                {/* <Button color="inherit">Login</Button> */}
                             </Toolbar>
                         </AppBar>
                     </Box>
                 </div>
 
-                <div className="todo_list">
-                    todolist
+                <div className="todo_list_body">
+                <h2>To do list</h2>
+                    <div className="todo_input">
+                        <Box
+                        sx={{
+                            '& > :not(style)': { m: 1, width: '25ch' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                        >
+                            <TextField id="standard-basic" label="What's the to do?" variant="standard" />
+                        </Box>
+                    </div> 
+                    
+                    <div className="list_wrap">
+                        <ul className="todo_list">
+                            <li>Kill'em with the bass</li>
+                            <li>Dead beats Listen up</li>
+                        </ul>
+                    </div>
                 </div>
+
             </div>
         </div>
     );
 }
-
-function Get_weather(){
-    if(!navigator.geolocation) return "X";
-
-    navigator.geolocation.getCurrentPosition(
-        (position) => {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=c8899082aee3315b60daa2a1da712285`)
-            .then((res) => {return res.json()})
-            .then((json) => {
-                let temp = ((json.main.temp - 273.15).toFixed(0));
-                console.log(temp);
-                return temp;
-            });
-        }
-    );
-
-    debugger;
-    return ww;
-};
-
-
-Get_weather();
 
 export default Todolist;
